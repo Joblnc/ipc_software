e5af3a28d4192ca215d7da6402831826d66d7459
Johan Blanc <blancjohan17@gmail.com>
2026-03-17 20:31:31 +0100

    changed previous to make it compatible with tcp server

--- arduino/script_clearer/script_clearer.ino
+++ arduino/script_clearer/script_clearer.ino
@@ -4,31 +4,100 @@
  */
 
 #include <SPI.h>
+#include <WiFiNINA.h>
+#include <WiFiUdp.h>
+#include <WiFiClient.h>
+
+// libraries specific to RP2040 : necessaries to generate pwm frequencies
 #include "hardware/pwm.h"
 #include "clocks.h" 
 
 // === CONFIGURATION ===
+// WiFi identifiers
+const char ssid[] = "AtelierLyon";     // to change
+const char pass[] = "AlexandreEstLePlusGrosBgDeCettePlanete!!"; // to change
+
+// IP Adresses
+// data collecter computer ip (maybe have to change it)
+const IPAddress PC_DATA_IP(10, 10, 1, 8); 
+const uint16_t PC_DATA_PORT_UDP = 12345;
+
+// server ip (maybe have to change it)
+const IPAddress PC_CONTROL_IP(10, 10, 1, 8);
+const uint16_t PC_CONTROL_PORT_TCP = 20000;
+
+// Hardware Pins
 const uint8_t PIN_ADC_INPUT = A0;   
 const uint8_t PIN_PWM_OUTPUT = 6;  // Remplacé par D6 (GPIO 6 natif sur RP2040)
 const uint8_t LED_Witness = 12;     
 
+// === DATA STRUCTURES ===
+
+// Structure that defines a specific frequency for the pwm (Pulse Width Modulation)
+// Goal of the structure : dividing the frequence of RP2040 (125 MHz) to send somthing 
+// reasonable to the plant
 typedef struct {
-  uint16_t top;       
-  uint8_t div_int;    
-  uint8_t div_frac;   
+  uint16_t top;       // max value of the counter, defines the period
+  uint8_t div_int;    // divides the frequency by an integer
+  uint8_t div_frac;   // adjusts the previous result by dividing with a float
 } pwm_config_t;
 
+// Arrays to stock sent frequencies and answers gotten
 #define MAX_FREQUENCIES 700
 pwm_config_t frequency_list[MAX_FREQUENCIES] = {};
 static uint16_t measurement_buffer[MAX_FREQUENCIES];
-static uint16_t baseline_buffer[MAX_FREQUENCIES]; // NOUVEAU : Mémoire de référence
+static uint16_t baseline_buffer[MAX_FREQUENCIES]; // Reference memory
 
-uint16_t delay_between_freqs_us = 1000; 
-uint16_t frequency_count = 0;           
+// Global Variables
+WiFiUDP udp;
+WiFiClient tcpClient;
+int wifiStatus = WL_IDLE_STATUS;
+
+// Measurement parameters (editable by the TCP server) 
+uint16_t delay_between_freqs_us = 1000;  // time to wait between each signal that we send (in micro seconds)
+uint16_t frequency_count = 0;            // number of frequency analysed
+bool isScanning = false;                 // System state (on / off)
 uint pwm_slice;                         
+
+// States of the arduino
+enum State {
+  WAITING_FOR_COMMAND,
+  RECEIVING_FREQUENCY_LIST
+};
+State currentState = WAITING_FOR_COMMAND;
+
+// Debug variable, for visual result on the serial plotter
+bool debug = 0;
 
 // === UTILS FUNCTIONS ===
 
+// function to connect to wifi with blinking led
+void connectToWiFi() {
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  
+  // if red is on, then not connected
+  digitalWrite(LEDR, HIGH); 
+  digitalWrite(LEDG, LOW);
+
+  Serial.print("connexion attempt to SSID: ");
+  Serial.println(ssid);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    WiFi.begin(ssid, pass);
+    // blinking while waiting for connexion
+    digitalWrite(LEDR, LOW); delay(250);
+    digitalWrite(LEDR, HIGH); delay(250);
+    Serial.print(".");
+  }
+
+  // green on => connected
+  Serial.println("\nConnected to WiFi !");
+  digitalWrite(LEDR, LOW);
+  digitalWrite(LEDG, HIGH);
+}
+
+// Configuration of the pwm on the RP2040
 void configurePWM(uint slice, uint16_t top, uint8_t div_int, uint8_t div_frac) {
   pwm_set_counter(slice, 0);
   pwm_set_clkdiv_int_frac(slice, div_int, div_frac);
@@ -36,70 +105,174 @@ void configurePWM(uint slice, uint16_t top, uint8_t div_int, uint8_t div_frac) {
   pwm_set_chan_level(slice, PWM_CHAN_A, (top + 1) / 2); 
 }
 
+// principal function : sends the different frequences to the plant and read the answers
 void performFrequencySweep(uint slice, uint16_t* buffer_to_fill) {
-  pwm_set_enabled(slice, true);
-  digitalWrite(LED_Witness, HIGH);
+  pwm_set_enabled(slice, true);     // activates the pwm generator
+  digitalWrite(LED_Witness, HIGH);  // turns on the witness led
 
+  // iterates over the frequency list
   for (size_t i = 0; i < frequency_count; i++) {
+    // configures the current frequency
     auto &cfg = frequency_list[i];
     configurePWM(slice, cfg.top, cfg.div_int, cfg.div_frac);
+
+    // waits for the signal to stabilize
     delayMicroseconds(delay_between_freqs_us);
     
-    // Remplit le tableau qu'on lui a passé en paramètre
+    // fills the array with answers of the plant
     buffer_to_fill[i] = analogRead(PIN_ADC_INPUT);
   }
 
-  digitalWrite(LED_Witness, LOW);
+  digitalWrite(LED_Witness, LOW);  // Turns off the led
   pwm_set_enabled(slice, false); 
 }
 
 // === SETUP ===
 
 void setup() {
+  // Pins initialization
   pinMode(LED_Witness, OUTPUT);
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+
   Serial.begin(115200); 
   
-  // Petite pause pour laisser le temps d'ouvrir le Traceur Série
+  // Cheks the FirmWare WiFi
+  if (WiFi.firmwareVersion() < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("ATTENTION: Please update the FirmWare wifi");
+  }
+
+  // WiFi Connection
+  connectToWiFi();
+
+  // PWM pin configuration (18, or A2)
+  gpio_set_function(PIN_PWM_OUTPUT, GPIO_FUNC_PWM);
+
+  // small break to let time to open serial tracer
   delay(3000); 
 
+  // PWM pin configuration (18, or A2)
   gpio_set_function(PIN_PWM_OUTPUT, GPIO_FUNC_PWM);
+
+  // ADC configuration : 12 bits resolution => values from 0 to 4095
   analogReadResolution(12); 
+
+  // UDP Start
+  udp.begin(PC_DATA_PORT_UDP);
+
   pwm_slice = pwm_gpio_to_slice_num(PIN_PWM_OUTPUT);
 
-  // Génération des 24 fréquences
-  for (uint32_t freq = 20000; freq <= 250000; freq += 10000) {
-    frequency_list[frequency_count].top = (125000000 / freq) - 1;
-    frequency_list[frequency_count].div_int = 1;
-    frequency_list[frequency_count].div_frac = 0;
-    frequency_count++;
+  if (debug == 1){
+    // generates specific frequences
+    for (uint32_t freq = 20000; freq <= 250000; freq += 10000) {
+      frequency_list[frequency_count].top = (125000000 / freq) - 1;
+      frequency_list[frequency_count].div_int = 1;
+      frequency_list[frequency_count].div_frac = 0;
+      frequency_count++;
+    }
+    
+    // Reference line calibration : first sweep on nothing 
+    performFrequencySweep(pwm_slice, baseline_buffer);
   }
-  
-  // --- NOUVEAU : CALIBRATION DE LA LIGNE DE BASE ---
-  // On fait un premier balayage "à vide" et on le sauvegarde dans le baseline_buffer
-  performFrequencySweep(pwm_slice, baseline_buffer);
 }
 
 // === LOOP ===
 
 void loop() {
-  // 1. Fait une nouvelle mesure et la stocke dans le measurement_buffer
-  performFrequencySweep(pwm_slice, measurement_buffer);
-  
-  // 2. Affiche la DIFFÉRENCE
-  for (size_t i = 0; i < frequency_count; i++) {
-    // Calcul de la différence (on utilise 'int' car le résultat peut être négatif)
-    int difference = (int)measurement_buffer[i] - (int)baseline_buffer[i];
 
-    // On fige l'axe Y très serré autour de 0 pour faire l'effet "loupe"
-    Serial.print("Min:-150, ");
-    Serial.print("Max:150, ");
-    Serial.print("Zero_Ref:0, "); // Dessine une ligne plate à 0 pour repère visuel
-    
-    Serial.print("Variation:");
-    Serial.println(difference);
-    
-    delay(20); 
+  // Reconnects automatically to wifi if it lost connection
+  if (WiFi.status() != WL_CONNECTED) {
+    connectToWiFi();
+    tcpClient.stop(); 
   }
+
+  // TCP connection management : while we are not connected, we try to connect to the 
+  // TCP server again
+  if (!tcpClient.connected()) {
+    Serial.println("Connection attempt ...");
+    tcpClient.connect(PC_CONTROL_IP, PC_CONTROL_PORT_TCP);
+    delay(1000); // 1s delay to not spam
+    return;
+  }
+
+  // Reads TCP commands
+  if (tcpClient.available()) {
+    
+    if (currentState == WAITING_FOR_COMMAND) {
+      uint8_t command = tcpClient.read();
+      
+      switch (command) {
+        case 0: // STOP
+          isScanning = false;
+          Serial.println("CMD: Stop Scanning");
+          break;
+          
+        case 1: // START
+          isScanning = true;
+          Serial.println("CMD: Start Scanning");
+          break;
+          
+        case 2: // SET DELAY
+          // Reads 2 bytes to form a uint16_t
+          tcpClient.readBytes((char*)&delay_between_freqs_us, 2);
+          Serial.print("CMD: Delay set to "); Serial.println(delay_between_freqs_us);
+          break;
+          
+        case 3: // LOAD FREQUENCIES
+          currentState = RECEIVING_FREQUENCY_LIST; // Switch state to receive big list
+          Serial.println("CMD: Loading Frequencies...");
+          break;
+      }
+    }
+    
+    else if (currentState == RECEIVING_FREQUENCY_LIST && debug == 0) {
+      // Reads the number of frequencies we'll get, on 2 bytes
+      if (tcpClient.available() >= 2) {
+         tcpClient.readBytes((char*)&frequency_count, 2);
+         if(frequency_count > MAX_FREQUENCIES) frequency_count = MAX_FREQUENCIES; // Max value if it is asked for more
+      }
+      
+      // fills the array with what is asked by the server
+      // note : not professional, it is waiting to receive all data : maybe we should add a timeout
+      uint16_t loaded = 0;
+      while (loaded < frequency_count) {
+        if (tcpClient.available() >= sizeof(pwm_config_t)) {
+          tcpClient.readBytes((char*)&frequency_list[loaded], sizeof(pwm_config_t));
+          loaded++;
+        }
+      }
+      Serial.println("Frequencies Loaded.");
+      currentState = WAITING_FOR_COMMAND; // Indicates that frequencies are loaded
+    }
+  }
+
+  if (debug == 1 || isScanning){
+    // stores new measures in measurment buffer
+    performFrequencySweep(pwm_slice, measurement_buffer);
+    
+    // displays difference between measurements and references
+    for (size_t i = 0; i < frequency_count; i++) {
+      // difference calculation
+      int difference = (int)measurement_buffer[i] - (int)baseline_buffer[i];
+
+      // set y axis around 0 to make a zoom effect
+      Serial.print("Min:-150, ");
+      Serial.print("Max:150, ");
+      Serial.print("Zero_Ref:0, "); // Draws a line on 0 as reference
+      
+      Serial.print("Variation:");
+      Serial.println(difference);
+      
+      delay(20); 
+    }
+
+    // send the results (i.e. the answers of the plant to each frequency)
+    udp.beginPacket(PC_DATA_IP, PC_DATA_PORT_UDP);
+    udp.write((uint8_t*)measurement_buffer, frequency_count * sizeof(uint16_t));
+    udp.endPacket();
+  }
+  
 
   // Pause de 2 secondes
   delay(2000); 
